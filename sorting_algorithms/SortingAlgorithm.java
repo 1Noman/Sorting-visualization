47f5e501bb9ea03e76c4c1921a02aa3af54c821d
Mohammad Noman <126937468+1Noman@users.noreply.github.com>
2023-06-01 20:49:21 +0600

    Add files via upload

---
+++ sorting_algorithms/SortingAlgorithm.java
@@ -0,0 +1,33 @@
+package sorting_algorithms;
+
+import drawing.PaintSurface;
+import gui_components.GUIComponents;
+
+/**
+ * @author Daniel
+ * 
+ * Template for the sorting algorithms.
+ */
+
+public abstract interface SortingAlgorithm {
+    public abstract String getName();
+
+    public abstract void doSort(int[] nums);
+
+    public abstract void changeDelay(int delay);
+
+    public static void setCurrentBar(int currentBarIndex) {
+        PaintSurface.currentBarIndex = currentBarIndex;
+    }
+
+    public static void sleepFor(int delay) {
+        long timeElapsed;
+        final long startTime = System.nanoTime();
+
+        do {
+            timeElapsed = System.nanoTime() - startTime;
+        } while(timeElapsed < delay);
+
+        GUIComponents.displayPanel.repaint();
+    }
+}
